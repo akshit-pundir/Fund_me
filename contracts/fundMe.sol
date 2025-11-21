63ba611ac64bf932ddb35a9082f1a4396051decd
akshit-pundir <akshit.pundir90@gmail.com>
2025-11-21 18:56:15 +0530

    getConversion func

--- contracts/fundMe.sol
+++ contracts/fundMe.sol
@@ -5,15 +5,20 @@ import { AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/shared/inte
 
 contract fundeMe {
     
-    uint256 public minimumUsd = 5;
+    uint256 public minimumUsd = 5; // same as 5e18
+    
+    address[] public funders;
+    mapping (address funder => uint256 amountFunded) public addressToAmount;
+    uint256 public val;
 
     function fund() public payable {
         // allow user to send money
         // have a minimum $ sent
 
-        require(msg.value > minimumUsd, "didn't send enough ETH" ); // 1e18 = 1 ETH = 1 * 10 ** 18 wei
-        
-
+        val = msg.value;
+        require( getConversionRate(msg.value/1e18) >= minimumUsd, "didn't send enough ETH" ); // 1e18 = 1 ETH = 1 * 10 ** 18 wei
+        funders.push(msg.sender);
+        addressToAmount[msg.sender] = addressToAmount[msg.sender] + getConversionRate(msg.value/1e18);
     }
 
     // function withdraw() public {}
@@ -24,9 +29,16 @@ contract fundeMe {
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         (,int256 answer,,,) = priceFeed.latestRoundData();
         return uint256(answer * 1e10);
+        
     }
 
-    function getConversionRate() internal {}
+    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
+        
+        uint256 ethPrice = getPrice();
+        uint256 ethPriceInUsd = (ethPrice * ethAmount)/1e18;
+        return ethPriceInUsd;
+    }
+
 
 }
 
