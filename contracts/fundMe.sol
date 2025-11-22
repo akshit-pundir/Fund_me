ba402cb126ea7f374f6010cdd81b0a5eabd9aec6
akshit-pundir <akshit.pundir90@gmail.com>
2025-11-22 18:05:26 +0530

    library setup

--- contracts/fundMe.sol
+++ contracts/fundMe.sol
@@ -2,42 +2,28 @@
 pragma solidity 0.8.24;
 
 import { AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import { priceConverter } from './priceConverter.sol';
+
 
 contract fundeMe {
     
-    uint256 public minimumUsd = 5; // same as 5e18
+    using priceConverter for uint256;
+
+    uint256 public minimumUsd = 5; 
     
     address[] public funders;
     mapping (address funder => uint256 amountFunded) public addressToAmount;
     uint256 public val;
 
     function fund() public payable {
-        // allow user to send money
-        // have a minimum $ sent
-
-        val = msg.value;
-        require( getConversionRate(msg.value/1e18) >= minimumUsd, "didn't send enough ETH" ); // 1e18 = 1 ETH = 1 * 10 ** 18 wei
+                
+        require( msg.value.getConversionRate() >= minimumUsd, "didn't send enough ETH" ); // 1e18 = 1 ETH = 1 * 10 ** 18 wei
         funders.push(msg.sender);
-        addressToAmount[msg.sender] = addressToAmount[msg.sender] + getConversionRate(msg.value/1e18);
+        addressToAmount[msg.sender] = addressToAmount[msg.sender] + msg.value.getConversionRate()/1e16;
     }
 
     // function withdraw() public {}
 
-    function getPrice() public view returns (uint256) {
-        // address -> 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        // ABI
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (,int256 answer,,,) = priceFeed.latestRoundData();
-        return uint256(answer * 1e10);
-        
-    }
-
-    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
-        
-        uint256 ethPrice = getPrice();
-        uint256 ethPriceInUsd = (ethPrice * ethAmount)/1e18;
-        return ethPriceInUsd;
-    }
 
 
 }
