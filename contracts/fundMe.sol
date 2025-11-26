e303f227aa2035e7387cdac19ea75665608ede46
akshit-pundir <akshit.pundir90@gmail.com>
2025-11-26 12:16:31 +0530

    final commit (project end)

--- contracts/fundMe.sol
+++ contracts/fundMe.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 import {priceConverter} from "./priceConverter.sol";
+
 
 // constant,immutable <--- make the contract little bit gas efficient
 
--- contracts/priceConverter.sol
+++ contracts/priceConverter.sol
@@ -7,9 +7,10 @@ import { AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/shared/inte
 library priceConverter {
 
      function getPrice() internal view returns (uint256) {
-        // address -> 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        // address -> 0x694AA1769357215DE4FAC081bf1f309aDC325306 => for sepolia
         // ABI
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        // AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xfEefF7c3fB57d18C5C6Cdd71e45D2D0b4F9377bF);
         (,int256 answer,,,) = priceFeed.latestRoundData();
         return uint256(answer * 1e10);
         
